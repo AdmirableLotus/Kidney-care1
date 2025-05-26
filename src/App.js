ee5beda77c6b10e9ab51e678a9307b4794f4eac1
Alena Kondratyeva <akondratyeva19@gmail.com>
2025-05-25 22:02:06 -0500

    modified:   src/App.js 	new file:   src/components/HeroLayout.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import PatientDashboard from './components/PatientDashboard';
 import StaffDashboard from './components/StaffDashboard';
 import HomePage from './components/HomePage';
+import Navbar from './components/Navbar'; // if you created a styled Navbar component
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/dashboard/patient" element={<PatientDashboard />} />
         <Route path="/dashboard/staff" element={<StaffDashboard />} />
-        <Route path="/" element={<h1>Welcome to Kidney Care</h1>} />
-        
       </Routes>
     </Router>
   );
