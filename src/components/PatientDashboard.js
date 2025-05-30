9fff2bedfa30d38a378bae24b503f0ef78e87962
Alena Kondratyeva <akondratyeva19@gmail.com>
2025-05-29 23:31:35 -0500

    modified:   src/components/PatientDashboard.js

--- src/components/PatientDashboard.js
+++ src/components/PatientDashboard.js
@@ -43,6 +43,14 @@ const PatientDashboard = () => {
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
       setUser(JSON.parse(storedUser));
+    } else {
+      // Try to fetch user from token if not in localStorage
+      const token = localStorage.getItem('token');
+      if (token) {
+        axios.get('http://localhost:5000/api/auth/me', {
+          headers: { Authorization: `Bearer ${token}` }
+        }).then(res => setUser(res.data)).catch(() => {});
+      }
     }
   }, []);
 
