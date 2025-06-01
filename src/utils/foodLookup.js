ccd822aa95d1e9d8744efa8901d2b5133c5ba15d
Alena Kondratyeva <akondratyeva19@gmail.com>
2025-06-01 17:38:53 -0500

    new file:   src/utils/foodLookup.js

---
+++ src/utils/foodLookup.js
@@ -0,0 +1,50 @@
+export const foodDatabase = {
+  "toast with butter": {
+    protein: 3,
+    phosphorus: 45,
+    potassium: 50,
+    sodium: 200
+  },
+  "scrambled eggs": {
+    protein: 6,
+    phosphorus: 100,
+    potassium: 70,
+    sodium: 150
+  },
+  "oatmeal": {
+    protein: 5,
+    phosphorus: 180,
+    potassium: 150,
+    sodium: 50
+  },
+  "banana": {
+    protein: 1,
+    phosphorus: 20,
+    potassium: 360,
+    sodium: 1
+  },
+  "chicken breast": {
+    protein: 31,
+    phosphorus: 200,
+    potassium: 220,
+    sodium: 65
+  },
+  "rice": {
+    protein: 4,
+    phosphorus: 100,
+    potassium: 80,
+    sodium: 5
+  },
+  "salmon": {
+    protein: 22,
+    phosphorus: 240,
+    potassium: 380,
+    sodium: 50
+  },
+  "greek yogurt": {
+    protein: 10,
+    phosphorus: 135,
+    potassium: 140,
+    sodium: 50
+  }
+};
