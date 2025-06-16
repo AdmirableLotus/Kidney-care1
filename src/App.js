ccb6cf683ef55737f1afb6844afc394eb8f6461e
Alena Kondratyeva <akondratyeva19@gmail.com>
2025-06-15 19:06:03 -0500

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
+import StaffLoginForm from './components/StaffLoginForm';
+import StaffRegisterForm from './components/StaffRegisterForm';
 import PatientDashboard from './components/PatientDashboard';
 import StaffDashboard from './components/StaffDashboard';
 import HomePage from './components/HomePage';
-import React from 'react';
-
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <div className="page-background" />
+
         <Routes>
+          {/* Public routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
-          <Route path="/dashboard/patient" element={<PatientDashboard />} />
-          <Route path="/dashboard/staff" element={<StaffDashboard />} />
+
+          {/* Staff-specific auth */}
+          <Route path="/staff/login" element={<StaffLoginForm />} />
+          <Route path="/staff/register" element={<StaffRegisterForm />} />
+
+          {/* Protected dashboards */}
+          <Route
+            path="/dashboard/patient"
+            element={
+              <PrivateRoute>
+                <PatientDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard/staff"
+            element={
+              <PrivateRoute>
+                <StaffDashboard />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Catch-all redirect */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
